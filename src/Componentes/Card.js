e2c980814ae4e3461aca334b7f2acd5a0ca425a5
unknown <nahomysalazar15@gmail.com>
2024-03-21 12:45:30 -0800

    Sumar/Restar

--- src/Componentes/Card.js
+++ src/Componentes/Card.js
@@ -1,18 +1,61 @@
+import { useState } from 'react';
 import '../Estilos/Card.css'
+
+
 
 export function Card(props){
 
+  
+    const[producto,SetPrice] = useState({category: props.producto.category,
+        id: props.producto.id,
+        price: props.producto.price,
+        cantidad: 0})
+
+
+        const Sumar = () => {
+
+            SetPrice((producto) => {
+                return {...producto, cantidad: producto.cantidad + 1}
+            })
+        }
+
+        const Restar = () => {
+
+            if(producto.cantidad == 0){
+                SetPrice((producto) => {
+                    return {...producto, cantidad: 0 }
+                })
+            }
+            else{
+            
+                SetPrice((producto) => {
+                    return {...producto, cantidad: producto.cantidad - 1}
+                })
+            }
+
+        }
+    
+
  return(
- <div className='Card col-2'>
+ <div className='Card col-2 d-flex' style={{flexDirection: 'column', justifyContent: 'space-around'}}>
     <div className='d-flex justify-content-center mt-3'>
         <div style={{height:'150px', width:'170px', backgroundColor:'pink'}}>
             <img src={props.producto.image} style={{height:'100%', width:'100%'}}></img>
         </div>
     </div>
 
-    <div>
-        <h5>{props.producto.title}</h5>
-        <h6>{props.producto.price} usd</h6>
+    <div className='d-flex justify-content-center pt-3'>
+        <h5 className='Pfont-size trucar-text'>{props.producto.title}</h5>
+    </div>
+
+    <div className='d-flex justify-content-center pt-3'>
+         <button type="button" class="btn btn-primary size-button bg-secondary" onClick={() => Restar()} style={{border:'none'}}>-</button>
+         <input class="form-control mx-3 w-25 text-center" type="text" value={producto.cantidad} aria-label="readonly input example" ></input>
+         <button type="button " class="btn btn-primary size-button" onClick={() => Sumar()} style={{border:'none'}} >+</button>
+    </div>
+
+    <div className='d-flex justify-content-center'>
+        <h6 >{props.producto.price} usd</h6>
     </div>
 </div>
  );
--- src/Componentes/Departamento.js
+++ src/Componentes/Departamento.js
@@ -7,7 +7,6 @@ export function Departamento(props)
     const [ListProducts,SetListProducts] = useState([])
     
     useEffect(()=>{
-
         fetch('https://fakestoreapi.com/products/category/'+ props.NombreDpto).then(value => value.json()).then((value)=> {
         SetListProducts(value)
         })
@@ -19,7 +18,7 @@ export function Departamento(props)
     <div id={props.NombreDpto}>
       <div class="d-flex flex-wrap justify-content-between align-items-center pt-1 border-bottom pb-4 mb-4">
         <h2 class="h3 mb-0 pt-3 me-3">{props.NombreDpto}</h2>
-        <div class="pt-3"><a class="btn btn-outline-accent btn-sm" href="grocery-catalog.html">More products<i class="ci-arrow-right ms-1 me-n1"></i></a></div>
+        
      </div>
       {ListProducts.length > 0 ? <GridProducts ListaProductos = {ListProducts} /> :<div class="spinner-border text-primary" role="status"><span class="sr-only"></span>
 </div>}
